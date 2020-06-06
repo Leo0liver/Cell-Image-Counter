14723e5de3fe699aa93182f2de537c76fad3346e
sm472 <>
2020-06-06 16:08:11 +1200

    Added MedianFilter and Sharpening programs

---
+++ src/MedianFilter.java
@@ -0,0 +1,63 @@
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.awt.Color;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
+
+
+class MedianFilter{
+    int width;
+    int height;
+    BufferedImage image;
+    BufferedImage image_copy;
+    float[][] filter = {{1,2,1},{2,3,3},{1,2,1}};
+    public static void main(String [] args)throws IOException
+    {
+         if(args.length!=1)
+         {
+            System.out.println("invalid input");
+            System.exit(0);
+         } 
+         MedianFilter s = new MedianFilter();
+         s.process(args[0]);
+    }
+
+    public void process(String file)throws IOException
+    {
+        Color[] pixel=new Color[9];
+        int[] R=new int[9];
+        int[] B=new int[9];
+        int[] G=new int[9];
+        File output=new File("denoise.jpg");
+        BufferedImage img=ImageIO.read(new File(file));
+        for(int i=1;i<img.getWidth()-1;i++)
+            for(int j=1;j<img.getHeight()-1;j++)
+            {
+               pixel[0]=new Color(img.getRGB(i-1,j-1));
+               pixel[1]=new Color(img.getRGB(i-1,j));
+               pixel[2]=new Color(img.getRGB(i-1,j+1));
+               pixel[3]=new Color(img.getRGB(i,j+1));
+               pixel[4]=new Color(img.getRGB(i+1,j+1));
+               pixel[5]=new Color(img.getRGB(i+1,j));
+               pixel[6]=new Color(img.getRGB(i+1,j-1));
+               pixel[7]=new Color(img.getRGB(i,j-1));
+               pixel[8]=new Color(img.getRGB(i,j));
+               for(int k=0;k<9;k++){
+                   R[k]=pixel[k].getRed();
+                   B[k]=pixel[k].getBlue();
+                   G[k]=pixel[k].getGreen();
+               }
+               Arrays.sort(R);
+               Arrays.sort(G);
+               Arrays.sort(B);
+               img.setRGB(i,j,new Color(R[4],B[4],G[4]).getRGB());
+            }
+        ImageIO.write(img,"jpg",output);
+   }
+   
+}
---
+++ src/sharpen.java
@@ -0,0 +1,36 @@
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
+
+
+class sharpen
+{
+    int width;
+    int height;
+    BufferedImage image;
+    BufferedImage image_copy;
+    
+    public static void main(String [] args)throws IOException
+    {
+         if(args.length!=1)
+         {
+            System.out.println("invalid input");
+            System.exit(0);
+         } 
+         sharpen s = new sharpen();
+         s.process(args[0]);
+    }
+
+    public void process(String file)throws IOException
+    {   
+      File f = new File(file);
+      image = ImageIO.read(f);
+      Kernel kernel = new Kernel(3, 3, new float[] { -1, -1, -1, -1, 9, -1, -1,-1,-1});
+      BufferedImageOp op = new ConvolveOp(kernel);
+      image = op.filter(image_copy,null);
+      ImageIO.write(image,"JPG",new File("sharp.JPG"));
+    }
+}
