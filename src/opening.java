60dcb3e6db8a2e9f73eb642b179299cf07aecea4
sm472 <>
2020-06-10 17:13:58 +1200

    added in opening code

---
+++ src/opening.java
@@ -0,0 +1,44 @@
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
+
+
+class opening
+{
+    int width;
+    int height;
+    static BufferedImage image;
+    static BufferedImage image_copy;
+    
+    public static void main(String [] args)throws IOException
+    {
+         if(args.length!=1)
+         {
+            System.out.println("invalid input");
+            System.exit(0);
+         }
+         process(args[0]);
+    }
+
+    public static void process(String file)throws IOException
+    {    File f = new File(file);
+         image = ImageIO.read(f);
+        
+         
+         image_copy = image;
+        
+         Kernel kernel = new Kernel(3, 3, new float[] { 0, 1, 0, 1, 0, 1, 0,
+            1, 0});
+         BufferedImageOp op = new ConvolveOp(kernel);
+         image = op.filter(image_copy,null);
+
+         
+      
+        ImageIO.write(image,"JPG",new File("output.JPG"));
+    }
+   
+   
+}
